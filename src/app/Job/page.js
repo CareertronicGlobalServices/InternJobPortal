8daa52d847a48c13c1984032557b6ab38ddff81d
Sonam <sonam.panjwani29@gmail.com>
2026-02-09 17:34:17 +0530

    Rename2  Commit

--- src/app/Job/page.js
+++ src/app/Job/page.js
@@ -1,10 +1,10 @@
 import React from "react";
-import JobNavBar from "@/component/JobPortal/JobNavbar";
-import JobHero from "@/component/JobPortal/JobHero";
-import DreamJob from "@/component/JobPortal/DreamJob";
-import GoodCompany from "@/component/JobPortal/GoodCompany";
-import CompanyPage from "@/component/JobPortal/CompanyPage";
-import Carousel from "@/component/JobPortal/Crousel";
+import JobNavBar from "@/component/Jobportal/JobNavbar";
+import JobHero from "@/component/Jobportal/JobHero";
+import DreamJob from "@/component/Jobportal/DreamJob";
+import GoodCompany from "@/component/Jobportal/GoodCompany";
+import CompanyPage from "@/component/Jobportal/CompanyPage";
+import Carousel from "@/component/Jobportal/Crousel";
 
 const page = () => {
   return (
